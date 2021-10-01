2d0fdd58b06f1aec76fd25b1044e3eb333322deb
VladislavKonstantinovich <83119196+VladislavKonstantinovich@users.noreply.github.com>
2021-10-02 01:01:03 +0300

    Add files via upload

--- task-5.js
+++ task-5.js
@@ -1,17 +1,31 @@
+let salaries = {
+    VladP: 194,
+    VladV: 194,
+    Misha: 2,
+}
+
+let sum = Object.values(salaries);
+let sumi = 0;
+for(var i=0;i<sum.length;i++){
+    sumi = sumi + parseInt(sum[i]);
+}
+
+if (Object.keys(salaries).length === 0) {
+    sumi = 0;
+    alert(sumi);
+} else {
+    alert(sumi);
+}
+
+
+
 // let salaries = {
 //     VladP: 194,
 //     VladV: 194,
 //     Misha: 2,
 // }
 
-// const sumi = function() {
-  
-//     for (const argument of arguments) {
-//         console.log(argument);
-//     }
-// };
-
-// let sum = sumi(Object.values(salaries));
+// let sum = salaries.VladP + salaries.VladV + salaries.Misha;
 
 // if (Object.keys(salaries).length === 0) {
 //     sum = 0;
@@ -19,20 +33,3 @@
 // } else {
 //     alert(sum);
 // }
-
-
-
-let salaries = {
-    VladP: 194,
-    VladV: 194,
-    Misha: 2,
-}
-
-let sum = salaries.VladP + salaries.VladV + salaries.Misha;
-
-if (Object.keys(salaries).length === 0) {
-    sum = 0;
-    alert(sum);
-} else {
-    alert(sum);
-}
